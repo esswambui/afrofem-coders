9e8c2dbc554254befc3c5457dfe0b9c95615a988
esswanbui <estherwambuik3@gmail.com>
2021-12-13 00:29:04 +0300

    squares functionality

--- color-guessing-game/app.js
+++ color-guessing-game/app.js
@@ -4,6 +4,7 @@ const gButton = document.getElementById('g');
 const bButton = document.getElementById('b');
 
 let colorDisplay = document.querySelector("#color-display");
+let messageDisplay = document.querySelector("#message");
 
 // array of game levels html collection
 const levels = Array.from(document.getElementsByClassName('mode'));
@@ -23,7 +24,7 @@ let gameLevel = levels.find((level) => {
 // array of randomly generated colors
 let colors = generateRandomColors(squares.length);
 
-var pickedColor = randomColorPick();
+let pickedColor = randomColorPick();
 
 // Add event listener to each game level
 levels.forEach(level => {
@@ -44,7 +45,7 @@ levels.forEach(level => {
                 //reset winning color
                 pickedColor = randomColorPick();
                 //change display to show new picked color
-                colorDisplay.textContent = pickedColor;
+                //colorDisplay.textContent = pickedColor;
                 for(let i = 0; i < squares.length; i++){
                     if(colors[i]){
                         squares[i].style.background = colors[i];
@@ -60,7 +61,7 @@ levels.forEach(level => {
                 //reset winning color
                 pickedColor = randomColorPick();
                 //change display to show new picked color
-                colorDisplay.textContent = pickedColor;
+                //colorDisplay.textContent = pickedColor;
                 for(var i = 0; i < squares.length; i++){
                     squares[i].style.backgroundColor = colors[i];
                     squares[i].style.display = "block";
@@ -87,7 +88,7 @@ startButton.addEventListener("click", function () {
     //pick a new random color from array
     pickedColor = randomColorPick();
     //change colorDisplay to match picked color
-    colorDisplay.textContent = pickedColor;
+   // colorDisplay.textContent = pickedColor;
     startButton.textContent = "New Colors";
     //messageDisplay.textContent = "";
     	for (let i=0; i<squares.length; i++) {
@@ -96,15 +97,35 @@ startButton.addEventListener("click", function () {
         }
 })
 
-colorDisplay.textContent = pickedColor;
+//colorDisplay.textContent = pickedColor;
 
+for(let i = 0; i < squares.length; i++) {
+    //add initial colors to squares
+    //squares[i].style.backgroundColor = colors[i];
+    //add click listeners to squares    
+    squares[i].addEventListener("click", function(){
+    //grab color of clicked square
+    let clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    console.log(clickedColor, pickedColor);
+
+    if(clickedColor == pickedColor){
+        messageDisplay.textContent = "Correct!";
+        startButton.textContent = "Play Again?";
+        colorAll(clickedColor);
+     } else {
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
+        }
+    }
+    )}
 
 // function to generate random RGB colors
 function randomRGB () {
     const red = Math.floor(Math.random () *256);
     const green = Math.floor(Math.random () *256);
     const blue = Math.floor(Math.random () *256);
-    const rgbString = "rgb(" + red + "," + green + "," + blue + ")";
+    const rgbString = "rgb(" + red + ", " + green + ", " + blue + ")";
 
     return rgbString;
 }
@@ -128,5 +149,13 @@ function randomColorPick(){
         let randomIndex = Math.floor(Math.random() * colors.length)
         return colors[randomIndex];
         }
+
+function colorAll(color){
+            //loop through all squares
+            for(let i = 0; i < squares.length; i++){
+            //change each color to match given color
+            squares[i].style.background = color;
+            }
+            }
 
         
